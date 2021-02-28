161ce4c92a8919198f49a4e1a739cd98df8c0778
sudoutopia <79052452+sudoutopia@users.noreply.github.com>
2021-02-28 21:02:03 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>File Upload</title>
+  </head>
+  <body>
+      <h1> - File upload -</h1>
+      <br>
+      <form action="upload.php" method="POST" enctype="multipart/form-data">
+        Please upload your file here:<br>
+        <input type="file" name="fileUpload">
+        <br>
+        <br>  
+      <input type="submit">
+      </form>
+  </body>
+</html>
